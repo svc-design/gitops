e0b68c24d0cffda2e8c65094ed25d0027825d2f3
shenlan <manbuzhe2009@qq.com>
2025-06-25 21:47:00 +0800

    feat: auto-select labring registry

--- scripts/deepflow/deploy-k8s.sh
+++ scripts/deepflow/deploy-k8s.sh
@@ -12,8 +12,11 @@ sealos load -i sealos-helm.tar
 sealos load -i sealos-k8s-1.25.16.tar
 
 # 单机部署(单机部署无需ssh密码，root用户本机直接执行即可)
+# 根据节点 IP 所在地区自动选择拉取镜像的仓库
+REGISTRY_PREFIX=$(dirname "$0")/../get_labring_registry.sh
+REGISTRY_PREFIX=$("$REGISTRY_PREFIX")
 sealos run \
-    registry.cn-shanghai.aliyuncs.com/labring/kubernetes:v1.25.16  \
-    registry.cn-shanghai.aliyuncs.com/labring/helm:v3.9.4          \
-    registry.cn-shanghai.aliyuncs.com/labring/calico:v3.24.1       \
+    ${REGISTRY_PREFIX}/kubernetes:v1.25.16  \
+    ${REGISTRY_PREFIX}/helm:v3.9.4          \
+    ${REGISTRY_PREFIX}/calico:v3.24.1       \
     --single
