9e5ba63dd0ebeeec28b166e0baff35f9b5977091
Haitao Pan <manbuzhe2009@qq.com>
2025-06-26 13:22:07 +0800

    fix: enable universe repo, use sudo for sealos, fix role order

--- playbooks/roles/vhosts/common/files/install-packages.sh
+++ playbooks/roles/vhosts/common/files/install-packages.sh
@@ -3,5 +3,6 @@
 export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor > /usr/share/keyrings/hashicorp-archive-keyring.gpg
 sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
+sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get install -y vault auditd uidmap fuse-overlayfs
